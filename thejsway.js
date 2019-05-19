5349a3d31d507039ceff32f90721cb4deff21cd1
quintanamartin <quintana.martin1988@gmail.com>
2019-05-19 19:31:14 -0300

    added from celsius to fahrenheit

--- thejsway.js
+++ thejsway.js
@@ -22,3 +22,19 @@ function vat(){
    let vatRate = rawPrice * 0.206;
    console.log(`The VAT calculation is ${vatRate}`)
 }
+
+/*
+From Celsius to Fahrenheit degrees
+Write a program that asks
+for a temperature in Celsius degrees, then displays it in Fahrenheit degrees.
+
+The conversion between scales is given by the formula: [°F] = [°C] x 9 / 5 + 32.
+
+*/
+
+function celsiusToFahreinheit(){
+   let degrees = prompt('What is the temperature in Celsius degree?');
+   let fahrenheitDegrees = (degrees * 9 / 5) + 32;
+   return fahrenheitDegrees;
+}
+console.log(celsiusToFahreinheit());
