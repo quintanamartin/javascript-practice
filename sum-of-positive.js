7e2b07cbb97d067ced8cdda2025ff1a6322aef24
quintanamartin <quintana.martin1988@gmail.com>
2019-05-21 16:59:47 -0300

    add sum of positive

---
+++ sum-of-positive.js
@@ -0,0 +1,11 @@
+let arr = [1, -4, 7, 12];
+
+function positiveSum(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > 0) {
+      sum += arr[i];
+    }
+  }
+  return sum;
+}
